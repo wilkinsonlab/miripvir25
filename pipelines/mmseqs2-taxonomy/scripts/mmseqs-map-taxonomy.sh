4d70efac3cbb6a17c605e856ca7cf9c8fb8091ee
Bruno Cuevas <bruno.czuviria@upm.es>
2025-03-19 16:31:42 +0100

    minor bug fix

--- pipelines/mmseqs2-taxonomy/scripts/mmseqs-map-taxonomy.sh
+++ pipelines/mmseqs2-taxonomy/scripts/mmseqs-map-taxonomy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo {{upstream['mmseqs-create-db']['log']}}
-mmseqs2 taxonomy {{upstream['mmseqs-create-db']['db']}} {{ref_db_path}} {{name}} tmp -s 2 > {{product['log_search']}}
-mmseqs2 createtsv {{upstream['mmseqs-create-db']['db']}} {{name}} {{product['tsv']}} > {{product['log_tsv']}}
-mmseqs2 taxonomyreport {{upstream['mmseqs-create-db']}} {{name}} {{product['html']}} --report-mode 1  > {{product['log_report']}}
+mmseqs taxonomy {{upstream['mmseqs-create-db']['db']}} {{ref_db_path}} {{name}} tmp -s 2 > {{product['log_search']}}
+mmseqs createtsv {{upstream['mmseqs-create-db']['db']}} {{name}} {{product['tsv']}} > {{product['log_tsv']}}
+mmseqs taxonomyreport {{upstream['mmseqs-create-db']}} {{name}} {{product['html']}} --report-mode 1  > {{product['log_report']}}
