0c78f09e6ae4455031336e18e875cc75d4cb5689
Bruno Cuevas <bruno.czuviria@upm.es>
2025-03-24 19:41:34 +0100

    silly mistake

--- pipelines/mmseqs2-taxonomy/scripts/mmseqs-map-taxonomy.sh
+++ pipelines/mmseqs2-taxonomy/scripts/mmseqs-map-taxonomy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo {{upstream['mmseqs-create-db']['log']}}
-mmseqs taxonomy {{upstream['mmseqs-create-db']['db']}} {{ref_db_path}} {{name}} tmp -s 2 --split-memory-limit 128GB > {{product['log_search']}}
+mmseqs taxonomy {{upstream['mmseqs-create-db']['db']}} {{ref_db_path}} {{name}} tmp -s 2 --split-memory-limit 128G > {{product['log_search']}}
 mmseqs createtsv {{upstream['mmseqs-create-db']['db']}} {{name}} {{product['tsv']}} > {{product['log_tsv']}}
 mmseqs taxonomyreport {{ref_db_path}} {{name}} {{product['html']}} --report-mode 1  > {{product['log_report']}}
