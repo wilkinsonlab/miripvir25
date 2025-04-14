cfd2a0ca989e495afbfe502cebf29630ac27b515
Bruno Cuevas <bruno.czuviria@upm.es>
2025-04-14 21:00:48 +0200

    adapting mmseqs2 pipeline to left-over sequences

--- pipelines/mmseqs2-taxonomy/scripts/quality.sh
+++ pipelines/mmseqs2-taxonomy/scripts/quality.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 # Trims adapters and cuts regions with low quality
+primers="{{primers}}"
+if [ -z "${primers}" ]; then
+    fastp -i {{upstream['copy']['output_read1']}} \
+        -I {{upstream['copy']['output_read2']}} \
+        -q {{quality_threshold}} \
+        -w {{threads}} \
+        -o {{product['output_read1']}} -O {{product['output_read2']}} > {{product['log']}}
 
-fastp -i {{upstream['copy']['output_read1']}} \
-    -I {{upstream['copy']['output_read2']}} \
-    -a {{primers}} \
-    -q {{quality_threshold}} \
-    -w {{threads}} \
-    -o {{product['output_read1']}} -O {{product['output_read2']}} > {{product['log']}}
+else
+    fastp -i {{upstream['copy']['output_read1']}} \
+        -I {{upstream['copy']['output_read2']}} \
+        -a {{primers}} \
+        -q {{quality_threshold}} \
+        -w {{threads}} \
+        -o {{product['output_read1']}} -O {{product['output_read2']}} > {{product['log']}}
+fi
--- pipelines/mmseqs2-taxonomy/scripts/scp.sh
+++ pipelines/mmseqs2-taxonomy/scripts/scp.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 # Copies and inflates the file. We use pigz to speed it up.
 scp "{{user}}@{{url}}:{{remote_path}}/{{input_read1}}" . 
-pigz -d -c -p8 {{input_read1}} > {{product['output_read1']}}
-scp "{{user}}@{{url}}:{{remote_path}}/{{input_read2}}" . 
-pigz -d -c -p8 {{input_read2}} > {{product['output_read2']}}
+file="{{input_read1}}"
+if [ "${file: -3}" == ".gz" ]; then
+    pigz -d -c -p8 {{input_read1}} > {{product['output_read1']}}
+elif [ "${file: -4}" == ".zip" ]; then
+    unzip -p {{input_read1}} > {{product['output_read1']}}
+fi
 rm {{input_read1}}
+
+scp "{{user}}@{{url}}:{{remote_path}}/{{input_read2}}" . 
+file="{{input_read2}}"
+if [ "${file: -3}" == ".gz" ]; then
+    pigz -d -c -p8 {{input_read2}} > {{product['output_read2']}}
+elif [ "${file: -4}" == ".zip" ]; then
+    unzip -p {{input_read2}} > {{product['output_read2']}}
+fi
 rm {{input_read2}}
